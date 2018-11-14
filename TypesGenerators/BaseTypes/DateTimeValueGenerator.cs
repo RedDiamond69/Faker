58eaa2bf28e427637d093ad44a093f9f31decb6f
Aliaksei Kasiuk <reddiamond821@gmail.com>
2018-11-14 06:09:29 +0300

    Complete DateTimeValueGenerator class.

--- TypesGenerators/BaseTypes/DateTimeValueGenerator.cs
+++ TypesGenerators/BaseTypes/DateTimeValueGenerator.cs
@@ -8,5 +8,26 @@ namespace TypesGenerators.BaseTypes
 {
     public class DateTimeValueGenerator : IBaseGenerator
     {
+        private readonly Random _random;
+
+        public Type GenerateType { get; protected set; }
+
+        public DateTimeValueGenerator()
+        {
+            GenerateType = typeof(DateTime);
+            _random = new Random();
+        }
+
+        public object Generate()
+        {
+            int year = _random.Next(DateTime.MinValue.Year, DateTime.MaxValue.Year + 1);
+            int month = _random.Next(1, 12);
+            int day = _random.Next(1, DateTime.DaysInMonth(year, month) + 1);
+            int hour = _random.Next(0, 24);
+            int minute = _random.Next(0, 60);
+            int second = _random.Next(0, 60);
+            int millisecond = _random.Next(0, 1000);
+            return new DateTime(year, month, day, hour, minute, second, millisecond);
+        }
     }
 }
