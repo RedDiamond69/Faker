c4e476aa98e40c9bfd399010072f530533b9b30d
Aliaksei Kasiuk <reddiamond821@gmail.com>
2018-11-14 03:55:41 +0300

    Small fix for FakerConfig class.

--- Faker/FakerConfig.cs
+++ Faker/FakerConfig.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Linq.Expressions;
+using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -9,6 +10,8 @@ namespace Faker
 {
     public class FakerConfig : IFakerConfig
     {
+        public Dictionary<PropertyInfo, IBaseGenerator> Generators => throw new NotImplementedException();
+
         void IFakerConfig.Add<TType, TPropertyType, TGenerator>(Expression<Func<TType, TPropertyType>> expression)
         {
             throw new NotImplementedException();
