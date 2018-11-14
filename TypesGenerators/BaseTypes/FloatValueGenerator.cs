cfbb4e409bb003e0c3df404176ca1e09da6b6eab
Aliaksei Kasiuk <reddiamond821@gmail.com>
2018-11-14 06:13:59 +0300

    Complete FloatValueGenerator class.

--- TypesGenerators/BaseTypes/FloatValueGenerator.cs
+++ TypesGenerators/BaseTypes/FloatValueGenerator.cs
@@ -8,5 +8,19 @@ namespace TypesGenerators.BaseTypes
 {
     public class FloatValueGenerator : IBaseGenerator
     {
+        private readonly Random _random;
+
+        public Type GenerateType { get; protected set; }
+
+        public FloatValueGenerator()
+        {
+            GenerateType = typeof(float);
+            _random = new Random();
+        }
+
+        public object Generate()
+        {
+            return (float)_random.NextDouble();
+        }
     }
 }
