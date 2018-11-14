cdf3c12784cd38376e17d17f9616a69d554eaffa
Aliaksei Kasiuk <reddiamond821@gmail.com>
2018-11-14 04:55:41 +0300

    Created BoolValueGenerator class and implement inherited interface.

---
+++ TypesGenerators/BaseTypes/BoolValueGenerator.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TypesGenerators.BaseTypes
+{
+    public class BoolValueGenerator : IBaseGenerator
+    {
+        public Type GenerateType => throw new NotImplementedException();
+
+        public object Generate()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
