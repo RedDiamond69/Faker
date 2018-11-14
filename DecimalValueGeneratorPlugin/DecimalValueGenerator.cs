03444a28c9084b7ccdb1ab917fd8a43005dfd347
Aliaksei Kasiuk <reddiamond821@gmail.com>
2018-11-14 06:50:04 +0300

    DecimalValueGeneratorPlugin implemented.

--- DecimalValueGeneratorPlugin/DecimalValueGenerator.cs
+++ DecimalValueGeneratorPlugin/DecimalValueGenerator.cs
@@ -9,5 +9,19 @@ namespace DecimalValueGeneratorPlugin
 {
     public class DecimalValueGenerator : IBaseGenerator
     {
+        private readonly Random _random;
+
+        public Type GenerateType { get; protected set; }
+
+        public DecimalValueGenerator()
+        {
+            GenerateType = typeof(decimal);
+            _random = new Random();
+        }
+
+        public object Generate()
+        {
+            return (decimal)_random.NextDouble();
+        }
     }
 }
