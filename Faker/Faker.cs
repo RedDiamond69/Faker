ccfea776451d41a2d5950af79b28b006a962c24a
Aliaksei Kasiuk <reddiamond821@gmail.com>
2018-11-14 03:14:04 +0300

    Added base implementation for Faker class.

--- Faker/Faker.cs
+++ Faker/Faker.cs
@@ -1,12 +1,65 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
+using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace Faker
 {
-    public class Faker
+    public class Faker : IFaker
     {
+        private readonly string defPluginsFolder = "Extensions";
+
+        public Faker() : this(defPluginsFolder, null) { }
+
+        public Faker(string pluginsFolder) : this(pluginsFolder, null) { }
+
+        public Faker(IFakerConfig config) : this(defPluginsFolder, config) { }
+
+        public Faker(string pluginsFolder, IFakerConfig config)
+        {
+
+        }
+
+        public T Create<T>()
+        {
+            return (T)Create(typeof(T));
+        }
+
+        private object Create(Type type)
+        {
+            object generatedType;
+
+            return generatedType;
+        }
+
+        private object CreateByProperties(Type type)
+        {
+            object generatedType;
+
+            return generatedType;
+        }
+
+        private object CreateByConstractor(Type type, ConstructorInfo constructorInfo)
+        {
+            object generatedType;
+
+            return generatedType;
+        }
+
+        private bool CreateByCustomGenerator(PropertyInfo propertyInfo, out object generatedType)
+        {
+
+        }
+
+        private bool CreateByCustomGenerator(ParameterInfo parameterInfo, Type type, out object generatedType)
+        {
+
+        }
+
+        private bool CreateByCustomGenerator(FieldInfo fieldInfo, out object generatedType)
+        {
+
+        }
     }
 }
