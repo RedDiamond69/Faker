20380bce49646d91233fa861a0a4282632b4e092
Aliaksei Kasiuk <reddiamond821@gmail.com>
2018-11-14 05:11:06 +0300

    Complete IntValueGenerator class.

--- TypesGenerators/BaseTypes/IntValueGenerator.cs
+++ TypesGenerators/BaseTypes/IntValueGenerator.cs
@@ -8,5 +8,19 @@ namespace TypesGenerators.BaseTypes
 {
     public class IntValueGenerator : IBaseGenerator
     {
+        private readonly Random _random;
+
+        public Type GenerateType { get; protected set; }
+
+        public IntValueGenerator()
+        {
+            GenerateType = typeof(int);
+            _random = new Random();
+        }
+
+        public object Generate()
+        {
+            return _random.Next();
+        }
     }
 }
