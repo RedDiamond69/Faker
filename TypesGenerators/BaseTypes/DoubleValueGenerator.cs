0dd7e2f02fd880a75d83e0a03050d9333b7fcb6c
Aliaksei Kasiuk <reddiamond821@gmail.com>
2018-11-14 06:11:48 +0300

    Complete DoubleValueGenerator class.

--- TypesGenerators/BaseTypes/DoubleValueGenerator.cs
+++ TypesGenerators/BaseTypes/DoubleValueGenerator.cs
@@ -8,5 +8,19 @@ namespace TypesGenerators.BaseTypes
 {
     public class DoubleValueGenerator : IBaseGenerator
     {
+        private readonly Random _random;
+
+        public Type GenerateType { get; protected set; }
+
+        public DoubleValueGenerator()
+        {
+            GenerateType = typeof(double);
+            _random = new Random();
+        }
+
+        public object Generate()
+        {
+            return _random.NextDouble();
+        }
     }
 }
