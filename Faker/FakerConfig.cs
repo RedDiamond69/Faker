ed8ad4a1c3b7ea9c41b30b9c7bfc9a5df9096c3a
Aliaksei Kasiuk <reddiamond821@gmail.com>
2018-11-14 03:53:01 +0300

    Created FakerConfig class and added implementation for IFakerConfig interface.

---
+++ Faker/FakerConfig.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Faker
+{
+    public class FakerConfig : IFakerConfig
+    {
+        void IFakerConfig.Add<TType, TPropertyType, TGenerator>(Expression<Func<TType, TPropertyType>> expression)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
