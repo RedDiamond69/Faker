045e42ce8d37dce58de45d154a1f6c5ca21e674a
Aliaksei Kasiuk <reddiamond821@gmail.com>
2018-11-14 06:04:48 +0300

    Complete CharValueGenerator class.

--- TypesGenerators/BaseTypes/CharValueGenerator.cs
+++ TypesGenerators/BaseTypes/CharValueGenerator.cs
@@ -8,5 +8,21 @@ namespace TypesGenerators.BaseTypes
 {
     public class CharValueGenerator : IBaseGenerator
     {
+        private readonly Random _random;
+
+        public Type GenerateType { get; protected set; }
+
+        public CharValueGenerator()
+        {
+            GenerateType = typeof(char);
+            _random = new Random();
+        }
+
+        public object Generate()
+        {
+            char[] base64 = new char[4];
+            Convert.ToBase64CharArray(inArray: new byte[] { (byte)_random.Next() }, offsetIn: 0, length: 1, outArray: base64, offsetOut: 0);
+            return base64[_random.Next(0, 2)];
+        }
     }
 }
