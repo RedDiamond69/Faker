2f7d1cd11846381e47a1804d23021d67ca57abb6
Aliaksei Kasiuk <reddiamond821@gmail.com>
2018-11-14 09:43:24 +0300

    Created and added implementation for ConsoleJsonSerializer class.

---
+++ FakerConsole/Serializer/ConsoleJsonSerializer.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Runtime.Serialization.Json;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FakerConsole.Serializer
+{
+    public static class ConsoleJsonSerializer
+    {
+        public static void Serialize<T>(T toSerialize)
+        {
+            using (var jsonWriter = JsonReaderWriterFactory.CreateJsonWriter(Console.OpenStandardOutput(), Encoding.UTF8, ownsStream: true, indent: true))
+            {
+                new DataContractJsonSerializer(typeof(T)).WriteObject(jsonWriter, toSerialize);
+            }
+        }
+    }
+}
