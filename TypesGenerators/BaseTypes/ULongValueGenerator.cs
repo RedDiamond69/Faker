2cc8f01e25cb4baff9e00cbe63acc8cceafb3640
Aliaksei Kasiuk <reddiamond821@gmail.com>
2018-11-14 06:19:23 +0300

    Complete ULongValueGenerator class.

--- TypesGenerators/BaseTypes/ULongValueGenerator.cs
+++ TypesGenerators/BaseTypes/ULongValueGenerator.cs
@@ -8,5 +8,19 @@ namespace TypesGenerators.BaseTypes
 {
     public class ULongValueGenerator : IBaseGenerator
     {
+        private readonly Random _random;
+
+        public Type GenerateType { get; protected set; }
+
+        public ULongValueGenerator()
+        {
+            GenerateType = typeof(ulong);
+            _random = new Random();
+        }
+
+        public object Generate()
+        {
+            return (ulong)_random.NextDouble();
+        }
     }
 }
