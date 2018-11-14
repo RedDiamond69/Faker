8bc70643df2db125906034e1ea2e5f7b22ab0166
Aliaksei Kasiuk <reddiamond821@gmail.com>
2018-11-14 05:04:52 +0300

    Complete SByteValueGenerator class.

--- TypesGenerators/BaseTypes/SByteValueGenerator.cs
+++ TypesGenerators/BaseTypes/SByteValueGenerator.cs
@@ -8,6 +8,19 @@ namespace TypesGenerators.BaseTypes
 {
     public class SByteValueGenerator : IBaseGenerator
     {
+        private readonly Random _random;
 
+        public Type GenerateType { get; protected set; }
+
+        public SByteValueGenerator()
+        {
+            GenerateType = typeof(byte);
+            _random = new Random();
+        }
+
+        public object Generate()
+        {
+            return (sbyte)_random.Next();
+        }
     }
 }
