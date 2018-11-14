e34f03daf08691cc0cb989860b6ba4656e58c504
Aliaksei Kasiuk <reddiamond821@gmail.com>
2018-11-14 04:47:44 +0300

    Created IBaseGenerator interface and ByteValueGenerator class, added implementation for IBaseGenerator interface.

---
+++ TypesGenerators/BaseTypes/ByteValueGenerator.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TypesGenerators.BaseTypes
+{
+    public class ByteValueGenerator : IBaseGenerator
+    {
+        public Type GenerateType => throw new NotImplementedException();
+
+        public object Generate()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
