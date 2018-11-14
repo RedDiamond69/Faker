fa1f4340cdd388c1d64f0f1ad16406d5ed74cf70
Aliaksei Kasiuk <reddiamond821@gmail.com>
2018-11-14 05:13:41 +0300

    Complete UIntValueGenerator class.

--- TypesGenerators/BaseTypes/UIntValueGenerator.cs
+++ TypesGenerators/BaseTypes/UIntValueGenerator.cs
@@ -8,5 +8,19 @@ namespace TypesGenerators.BaseTypes
 {
     public class UIntValueGenerator : IBaseGenerator
     {
+        private readonly Random _random;
+
+        public Type GenerateType { get; protected set; }
+
+        public UIntValueGenerator()
+        {
+            GenerateType = typeof(uint);
+            _random = new Random();
+        }
+
+        public object Generate()
+        {
+            return (uint)_random.Next();
+        }
     }
 }
