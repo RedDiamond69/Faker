063a65447956bec90f7ca5fa3f4092b342cdc4be
Aliaksei Kasiuk <reddiamond821@gmail.com>
2018-11-14 09:21:15 +0300

    Added implementation for test classes.

---
+++ FakerConsole/TestClass/TestClassWithConstructor.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Runtime.Serialization;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FakerConsole.TestClass
+{
+    [DataContract]
+    public class TestClassWithConstructor : TestClassWithProperties
+    {
+        [DataMember]
+        public byte CustomGeneratorCheckProperty2 { get; set; }
+
+        public TestClassWithConstructor(byte byteValue, bool boolValue, byte customGeneratorCheckProperty1, byte customGeneratorCheckProperty2)
+        {
+            PublicByteSetter = byteValue;
+            publicBoolField = boolValue;
+            CustomGeneratorCheckProperty1 = customGeneratorCheckProperty1;
+            CustomGeneratorCheckProperty2 = customGeneratorCheckProperty2;
+        }
+    }
+}
---
+++ FakerConsole/TestClass/TestClassWithProperties.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Runtime.Serialization;
+using System.Xml;
+using System.Threading.Tasks;
+
+namespace FakerConsole.TestClass
+{
+    [DataContract]
+    public class TestClassWithProperties
+    {
+        [DataMember]
+        public bool publicBoolField;
+
+        [DataMember]
+        protected bool notPublicBoolField;
+
+        [DataMember]
+        public byte PublicByteSetter { get; set; }
+
+        [DataMember]
+        public byte NotPublicByteSetter { get; protected set; }
+
+        [DataMember]
+        private readonly byte notPublicByteField;
+
+        [DataMember]
+        public List<byte> publicList;
+
+        [DataMember]
+        public TestClassWithProperties nestedObject;
+
+        [DataMember]
+        public byte CustomGeneratorCheckProperty1 { get; set; }
+
+        public TestClassWithProperties() { }
+    }
+}
