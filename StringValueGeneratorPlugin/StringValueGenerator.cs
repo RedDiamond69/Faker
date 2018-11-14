c70401acfb5a3e6071d375ebabc346747ef2410b
Aliaksei Kasiuk <reddiamond821@gmail.com>
2018-11-14 06:55:05 +0300

    StringValueGeneratorPlugin implemented.

--- StringValueGeneratorPlugin/StringValueGenerator.cs
+++ StringValueGeneratorPlugin/StringValueGenerator.cs
@@ -9,5 +9,21 @@ namespace StringValueGeneratorPlugin
 {
     public class StringValueGenerator : IBaseGenerator
     {
+        private readonly Random _random;
+
+        public Type GenerateType { get; protected set; }
+
+        public StringValueGenerator()
+        {
+            GenerateType = typeof(string);
+            _random = new Random();
+        }
+
+        public object Generate()
+        {
+            byte[] str = new byte[_random.Next(0, byte.MaxValue)];
+            _random.NextBytes(str);
+            return Convert.ToBase64String(str);
+        }
     }
 }
