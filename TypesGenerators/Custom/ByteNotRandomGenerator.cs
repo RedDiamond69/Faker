b654fadfbb182b2424eec210a4895e1e9e3445d5
Aliaksei Kasiuk <reddiamond821@gmail.com>
2018-11-14 07:47:27 +0300

    Added implementation for ByteNotRandomGenerator class.

--- TypesGenerators/Custom/ByteNotRandomGenerator.cs
+++ TypesGenerators/Custom/ByteNotRandomGenerator.cs
@@ -3,10 +3,29 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using TypesGenerators.BaseTypes;
 
 namespace TypesGenerators.Custom
 {
-    public class ByteNotRandomGenerator : INotRandomGenerator
+    public class ByteNotRandomGenerator : IBaseGenerator, INotRandomGenerator
     {
+        public static int DefaultGeneratedValue { get => 255; }
+        public Type GenerateType { get; protected set; }
+        public int GenerateValue { get; protected set; }
+
+        public ByteNotRandomGenerator()
+            : this(DefaultGeneratedValue)
+        { }
+
+        public ByteNotRandomGenerator(int generatedValue)
+        {
+            GenerateType = typeof(int);
+            GenerateValue = generatedValue;
+        }
+
+        public object Generate()
+        {
+            return GenerateValue;
+        }
     }
 }
