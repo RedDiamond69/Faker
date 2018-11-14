6eea5bc2870a2648d7f2bfd937be99e6135f638b
Aliaksei Kasiuk <reddiamond821@gmail.com>
2018-11-14 04:05:59 +0300

    Complete FakerConfig class implementation.

--- Faker/FakerConfig.cs
+++ Faker/FakerConfig.cs
@@ -10,11 +10,22 @@ namespace Faker
 {
     public class FakerConfig : IFakerConfig
     {
-        public Dictionary<PropertyInfo, IBaseGenerator> Generators => throw new NotImplementedException();
+        private Dictionary<PropertyInfo, IBaseGenerator> _generators;
+
+        public Dictionary<PropertyInfo, IBaseGenerator> Generators { get => new Dictionary<PropertyInfo, IBaseGenerator>(_generators)}
+
+        public FakerConfig()
+        {
+            _generators = new Dictionary<PropertyInfo, IBaseGenerator>();
+        }
 
         void IFakerConfig.Add<TType, TPropertyType, TGenerator>(Expression<Func<TType, TPropertyType>> expression)
         {
-            throw new NotImplementedException();
+            Expression expressionBody = expression.Body;
+            if (expressionBody.NodeType != ExpressionType.MemberAccess) throw new ArgumentException("Unacceptable expression!\n");
+            IBaseGenerator generator = (IBaseGenerator)Activator.CreateInstance(typeof(TGenerator));
+            if (!generator.GeneratedType.Equals(typeof(TPropertyType))) throw new ArgumentException("Unacceptable generator!\n");
+            generators.Add((PropertyInfo)((MemberExpression)expressionBody).Member, generator);
         }
     }
 }
