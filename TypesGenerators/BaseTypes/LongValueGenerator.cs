a1ca9496700df3864f64c66393cdea031420aea2
Aliaksei Kasiuk <reddiamond821@gmail.com>
2018-11-14 06:17:30 +0300

    Complete LongValueGenerator class.

--- TypesGenerators/BaseTypes/LongValueGenerator.cs
+++ TypesGenerators/BaseTypes/LongValueGenerator.cs
@@ -8,5 +8,19 @@ namespace TypesGenerators.BaseTypes
 {
     public class LongValueGenerator : IBaseGenerator
     {
+        private readonly Random _random;
+
+        public Type GenerateType { get; protected set; }
+
+        public LongValueGenerator()
+        {
+            GenerateType = typeof(long);
+            _random = new Random();
+        }
+
+        public object Generate()
+        {
+            return (long)_random.NextDouble();
+        }
     }
 }
